0762cc3baeda0bfb0555fd6607c8dbc3cb2f55a2
Diego Muñoz <quantumjunkers288@gmail.com>
2026-03-30 07:39:23 -0600

    Primera versión: pantalla de inicio y navegación

---
+++ README.md
@@ -0,0 +1,3 @@
+# nursia_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/nursia_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nursia_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
